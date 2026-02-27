11c5f7698550db226f9fd2facf1698d22fc7c978
vivek kumar <vivekkumarfbg000@gmail.com>
2026-02-28 01:03:00 +0530

    fix(auth): add safe getallheaders polyfill to PHP proxy to prevent 500 HTML errors on Nginx/FPM (Hostinger)

--- public/supabase-proxy/index.php
+++ public/supabase-proxy/index.php
@@ -54,6 +54,24 @@ $targetUrl = SUPABASE_TARGET . $strippedPath;
 $skipHeaders = ['host', 'connection', 'accept-encoding', 'content-length', 'origin', 'referer'];
 $curlHeaders = [];
 
+// Safely get headers (getallheaders is Apache only, might crash on Nginx/FPM)
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headName = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))));
+                $headers[$headName] = $value;
+            } else if ($name == "CONTENT_TYPE") {
+                $headers["Content-Type"] = $value;
+            } else if ($name == "CONTENT_LENGTH") {
+                $headers["Content-Length"] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+
 foreach (getallheaders() as $name => $value) {
     if (in_array(strtolower($name), $skipHeaders)) continue;
     $curlHeaders[] = "{$name}: {$value}";
